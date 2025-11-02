97cb66d88051f4a2219c937214f24647067157c6
Nadeesha Hasaranga <hasaranganadeesha2004@gmail.com>
2025-11-02 22:23:36 +0530

    Created StaffMember class, with getters/setters..

---
+++ src/StaffMember.java
@@ -0,0 +1,60 @@
+import java.time.LocalDate;
+
+public class StaffMember {
+    protected String name;
+    protected String surname;
+    protected LocalDate dob;
+    protected String phoneNo;
+    protected String staffID;
+
+    public StaffMember(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String toString(){
+        return "";
+    }
+}
+
+
